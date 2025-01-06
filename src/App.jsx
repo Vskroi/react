136267e8475bbe0a0dae7805b3c1d10123a87dd8
Vskroi <24LP1862@G301-CC0201.local>
2025-01-06 18:06:14 +0800

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,78 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 import "./App.css";
 
 function App() {
-  const [newTask, setNewTask] = useState("")
   const [taskList, setTaskList] = useState([])
-  const onClick = () => {
-    setTaskList([...taskList, newTask])
-  }
+  const [error, setError] = useState(false)
+  const [newTask, setNewTask] = useState("")
+  const [filterState, setFilterState] = useState("ALL")
   const onChange = (e) => {
     setNewTask(e.target.value);
+  }
+  const handleAddTaskButton = () =>{
+    if (newTask.length === 0){
+      setError(true);
+    }else {
+      setError(false);
+      setTaskList([...taskList, {description:newTask, status:"ACTIVE", id: uuidv4()}]);
+      setNewTask("");
+    }
+    console.log(taskList)
+  }
+  const handleFilterStateChange = (state) =>{
+setFilterState(state)
   }
   return (
     <>
       <div className="app">
         <div className="content">
-          <p className="toDoList">To-Do list</p>
+          <b className="toDoList" >To-Do list</b>
+          {error && <div style={{color:"black"}}> Please enter a task!</div>}
           <div className="flex">
-            <input className="taskInput" type="text" placeholder="Add a new task..." id="input" onChange={onChange} />
-            <button className="taskButton" onClick={onClick} >
-              <p className="buttonText">Add</p>
+            <input className="taskInput" 
+            type="text" 
+            placeholder="Add a new task..." 
+            onChange={(onChange)}/>
+            
+            <button className="taskButton" onClick={handleAddTaskButton} >
+           Add
             </button>
             </div>
 
           <div className="taskDiv">
-            <button className="button1"><p>All</p></button>
-            <button className="button2"><p>Active</p></button>
-            <button className="button3"><p>Completed</p></button>
+            <div className="button1" onClick={() => handleFilterStateChange("ALL")} style={{color:filterState === "ALL" ?  "#3C82F6;":"#F3F4F6"}}>All</div>
+            <div className="button2" onClick={() => handleFilterStateChange("ACTIVE")} style={{background:filterState === "ACTIVE" ?  "#3C82F6;":"#F3F4F6"}}>Active</div>
+            <div className="button3" onClick={() => handleFilterStateChange("COMPLATE")} style={{background:filterState === "COMPLATE" ?  "#3C82F6;":"#F3F4F6"}}>Completed</div>
           </div>
-          {taskList.map((task, index) => (
-            <div key={index}>
-              <p style={{ color: "black" }}>{task}</p>
-            </div>
-          ))
+          {
+          taskList.filter((task) => {
+            if(filterState === "ALL"){
+              return true;
+            }
+            else if (filterState === task.state){
+              return true;
+            }
+          })
+          .map((task,index) => {
+return(
+            <div key={index} style={{display:"flex"}}>
+               <input type="checkBox" />
+              <p style={{ color: "black" }}>
+              {task.description}
+                </p>
+            </div>)
+
+})
           }
+     
           <p className="noTask">No tasks yet. Add one above!</p>
+    
           <div className="pinecone">
             <p className="powered">Powered by</p>
-            <a className="pineLInk" href="https://pinecone.mn/">Pinecone academy</a>
+            <a className="pineLink" href="https://pinecone.mn/">Pinecone academy</a>
           </div>
+
         </div>
       </div>
     </>
